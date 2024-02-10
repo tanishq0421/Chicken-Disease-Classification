f7912a8d896e3ff94b844e3e2462d70beb97d02d
tanishq <tanishq.shukla.cd.che21@iitbhu.ac.in>
2024-02-10 17:46:49 +0530

    stage_01_data_ingestion pipeline and main.py created

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 from chicken_disease_classification import logger
+from chicken_disease_classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hey there i run successfully")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<< \n\n\\x========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
